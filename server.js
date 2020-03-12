f36b507f893d3df4c79f4937d32276365cae3330
Shannon Sokolik <shannonsokolik@Shannons-MacBook-Air.local>
2020-03-12 16:18:28 -0500

    update server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express from "express"
+const express = require("express")
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/dist/SSPORTFOLIO"));
+app.use(express.static(__dirname + "/dist/"));
 
 // Allows for angular routing to take precedent
 app.get('*',  (req, res) => 
-      res.sendFile('/dist/SSPORTFOLIO/index.html', {root: __dirname + "/"}) 
+      res.sendFile('/dist/index.html', {root: __dirname + "/"}) 
 );
 
 
