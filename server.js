5c2c2917000ffc747a7e9b307fe143bcf1b3725b
Shannon Sokolik <shannonsokolik@Shannons-MacBook-Air.local>
2020-03-12 16:32:18 -0500

    server change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/dist"));
 
 // Allows for angular routing to take precedent
 app.get('*',  (req, res) => 
